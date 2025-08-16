b9adac75fbab740568c17f746d9c12f39c4dca5d
telluricaquarian <llewellyn@telluricaquarian.com>
2025-08-16 14:41:28 +1000

    Hero: update copy/styles

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -33,8 +33,8 @@ export default function HeroSection() {
   className="mb-12 text-lg tracking-tight text-gray-400 md:text-xl text-balance translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]"
   style={{ fontFamily: "Wednesday" }}
 >
-  The Philosophy of
-  <br className="hidden md:block" /> Monwhooper
+Welcome to monwhoopers repository of health, wealth & self. 
+  <br className="hidden md:block" /> Stay Dangerous.
 </p>
       <Button className="translate-y-[-1rem] animate-fade-in gap-1 rounded-lg text-white dark:text-black opacity-0 ease-in-out [--animation-delay:600ms]">
         <span>Get Started for free </span>
