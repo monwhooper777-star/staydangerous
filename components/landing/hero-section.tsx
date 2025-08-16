97918a2a2cf204338123581cba8b8664a36916c3
telluricaquarian <llewellyn@telluricaquarian.com>
2025-08-16 13:11:31 +1000

    Add Wednesday font utility and apply to hero h1 and p

--- components/landing/hero-section.tsx
+++ components/landing/hero-section.tsx
@@ -33,7 +33,7 @@ export default function HeroSection() {
   className="mb-12 text-lg tracking-tight text-gray-400 md:text-xl text-balance translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]"
   style={{ fontFamily: "Wednesday" }}
 >
-  THe Philosophy of
+  The Philosophy of
   <br className="hidden md:block" /> Monwhooper
 </p>
       <Button className="translate-y-[-1rem] animate-fade-in gap-1 rounded-lg text-white dark:text-black opacity-0 ease-in-out [--animation-delay:600ms]">
